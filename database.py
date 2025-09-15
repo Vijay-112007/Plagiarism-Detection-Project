f41bb66ea548e427135940e2b29e60a417cadcb6
K.Vijaya Sai <kattamudivijaysai@gmail.com>
2025-09-15 12:11:18 +0530

    Added code for two function
    Added code for seeValues and addValues

--- database.py
+++ database.py
@@ -16,10 +16,11 @@ class User:
         )
         self.cursorObject = self.dataBase.cursor() 
     def createDatabase(self,name):
-        self.query = "create database" + " " + name + " ;"
-        self.dataBase.commit()
+        self.query = "create database " + name + " ;"
         self.cursorObject.execute(self.query)
+        self.dataBase.commit()
         self.query = "use " + name + " ;"
+        self.cursorObject.execute(self.query)
         return (f"Database {name} is Created and Shifted to use")
     def seeDatabases(self):
         self.query = "show databases"
@@ -28,12 +29,32 @@ class User:
         for x in self.results:
             print(x)
     def createTable(self,table_name):
-        print(f"Creating a Table with Name {table_name}")
-        self.query = "create table" + " " + table_name + " " + "(student_id int primary key,student_name varchar(60) not null,student_branch varchar(40) not null,student_filename varchar(50),student marks double) values (%s,%s,%s,%s,%s)"
+        print(f"---Creating a Table with Name {table_name}---")
+        self.query = "create table" + " " + table_name + " " + "(student_id int primary key,student_name varchar(60) not null,student_branch varchar(40) not null,student_filename varchar(50),student_marks double)"
         self.cursorObject.execute(self.query)
+        self.dataBase.commit()
         return f"Table was created Successfully"
-    def addValues(self,values):
-        pass
+    def addValues(self,table_name,values):
+        #first we will ask the user to enter connect the database if connected no need
+        print(f"-----Printing the Table before Adding the Values----")
+        self.query = "select*from" + " " + table_name + ";"
+        self.cursorObject.execute(self.query)
+        self.results = self.cursorObjectfetchall()
+        for x in self.results:
+            print(x)
+        print(f"----Adding the Values----")
+        self.query = "insert into " + table_name + " (student_id,student_name,student_branch,student_filename,student_marks) values(%s,%s,%s,%s,%s)"
+        self.cursorObject.executemany(self.query,values)
+        self.dataBase.commit()
+        print("---Insertion Successfull---")
+    def seeValues(self,table_name):
+        #TO see the values or the entire table with the tuples 
+        self.query = "select*from" + " " + table_name + ";"
+        self.cursorObject.execute(self.query)
+        self.results = self.cursorObject.fetchall()
+        for x in self.results:
+            print(x)
+        print("===Values===")
     #Upto now this is the starting one
     
 def main():
