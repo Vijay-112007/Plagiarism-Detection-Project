1a294b8cc3a0845901e76867457822ef49839844
GowthamKC <93713826+GowthamKC@users.noreply.github.com>
2025-10-20 21:33:00 +0530

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -1,4 +1,7 @@
 import mysql.connector 
+import re
+import itertools
+
 DB_CONFIG = {
     "host": "localhost",
     "user": "root",           
@@ -6,4 +9,28 @@ DB_CONFIG = {
     "database": "PlagiarismStudents"
 }
 
-PREP_TABLE = "preprocessed_submissions"
+TABLE_NAME= "preprocessed_submissions"
+
+
+def kmp_match_count(document,snippet):
+    prefix=[0]*len(snippet)
+    build_prefix_table(snippet,prefix)
+    i=j=total=0
+
+   while i<len(document):
+       if snippet[j]==document[i]
+       i=i+1
+       j=j+1
+
+   if j==len(snippet)
+      total = total + 1
+      j=prefix[j-1]
+   elif i < len(document) and snippet[j] != document[i]:
+            if j != 0:
+                j = prefix[j - 1]
+            else:
+                i += 1
+    return total
+
+   
+    
