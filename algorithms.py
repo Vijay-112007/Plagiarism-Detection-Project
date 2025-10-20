a4442c74324f6786627cf0d6c150a299fcb589ae
GowthamKC <93713826+GowthamKC@users.noreply.github.com>
2025-10-20 21:16:50 +0530

    configuration

---
+++ algorithms.py
@@ -0,0 +1,9 @@
+import mysql.connector 
+DB_CONFIG = {
+    "host": "localhost",
+    "user": "root",           
+    "password": "PASSWORD",   
+    "database": "PlagiarismStudents"
+}
+
+PREP_TABLE = "preprocessed_submissions"
